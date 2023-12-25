406bca333299e801469fd2b47049bc71acb2beff
SonilaMertiri <sonilamertiri1@gmail.com>
2023-12-25 22:01:43 +0100

    assignment updatet with add a transfer_money method

--- Python/python-oop/user_bankAcc/user.py
+++ Python/python-oop/user_bankAcc/user.py
@@ -36,6 +36,23 @@ class User:
         # print(f"{self.name} made an {amount} leke withdrow.")
         # return self
     
+    
+    # Add a transfer_money(self, amount, other_user)
+    # method to the user class that takes an amount and a different User instance,
+    # and transfers money from the user's account into another user's account.
+
+    def transfer_money(self, amount, other_user, from_account_id, to_account_id):
+        if from_account_id in self.accounts and to_account_id in other_user.accounts:
+            if self.accounts[from_account_id].balance>= amount:
+                self.accounts[from_account_id].withdraw(amount)
+                other_user.accounts[to_account_id].deposit(amount)
+                print(f"{self.name} transferred {amount} from account {from_account_id} to {other_user.name} account {to_account_id}.")
+            else:
+                print(f"Transfer failed. Insufficient funds from {self.name} with account {from_account_id}")
+        else:
+            print("Unknown account information. Transfer failed.")
+        return self
+    
     def display_user_balance(self, account_id):
         if account_id in self.accounts:
             print(f"\n{self.name} your current balance for account {account_id} is: ")
@@ -47,12 +64,19 @@ class User:
         # print(f"\n{self.name} your current balance is:")
         # self.account.display_account_info()
         # return self
+    
 
 sonila= User("Sonila", "sonila@email.com")
-sonila_account1= sonila.add_acount("B1307", int_rate= 0.01, balance= 100)
-sonila_account2= sonila.add_acount("A2025", int_rate= 0.02, balance= 200)
+sonila_account1= sonila.add_acount("sonila1", int_rate= 0.01, balance= 100)
+sonila_account2= sonila.add_acount("sonila2", int_rate= 0.02, balance= 200)
 
-sonila.make_deposit("B1307", 70).make_withdraw("A2025", 20)
-sonila.display_user_balance("B1307").display_user_balance("A2025")
+sonila.make_deposit("sonila1", 70).make_withdraw("sonila2", 20)
+sonila.display_user_balance("sonila1").display_user_balance("sonila2")
 
 # sonila.make_deposit(100).make_withdrow(20).display_user_balance()
+
+paola= User("Paola", "paola@email.com")
+paola_account1= paola.add_acount("paola1", int_rate= 0.02, balance= 300)
+sonila.transfer_money(30, paola, "sonila2", "paola1")
+sonila.display_user_balance("sonila2").display_user_balance("sonila1")
+paola.display_user_balance("paola1")
