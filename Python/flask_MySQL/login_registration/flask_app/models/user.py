3ae91258f4c95743a7a049d0210ee626ee37db6b
SonilaMertiri <sonilamertiri1@gmail.com>
2024-01-30 11:11:39 +0100

    login registration form validations

---
+++ Python/flask_MySQL/login_registration/flask_app/models/user.py
@@ -0,0 +1,72 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re	# the regex module
+# create a regular expression object that we'll use later   
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+    DB = "logimishembull"
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password= data['password']
+        self.confirm_password= data['confirm_password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+    @classmethod
+    def create(cls,data):
+        query="INSERT INTO users (first_name, last_name, email, password, confirm_password) VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s, %(confirm_password)s);"
+        return connectToMySQL(cls.DB).query_db(query, data)
+    
+    @classmethod
+    def get_user_by_email(cls,data):
+        query= 'SELECT * FROM users WHERE email= %(email)s;'
+        result= connectToMySQL(cls.DB).query_db(query, data)
+        if result:
+            return result[0]
+        return False
+    
+
+    @staticmethod
+    def validate_userRegister(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", 'emailRegister')
+            is_valid = False
+        if len(user['password'])<=1:
+            flash("Password is required!", 'passwordRegister')
+            is_valid = False
+        if len(user['confirm_password'])<=1 or user['confirm_password'] != user['password']:
+            flash("Confirm password is incorrect!", 'passwordConfirm')
+            is_valid = False
+        if len(user['first_name'])<1:
+            flash("First name is required!", 'nameRegister')
+            is_valid = False
+        if len(user['last_name'])<1:
+            flash("Last name is required!", 'lastNameRegister')
+            is_valid = False
+        return is_valid
+#till now we made all the validations for user registrations
+
+    @staticmethod
+    def validate_user(user):
+        is_valid= True
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", 'emailLogin')
+            is_valid = False
+        if len(user['password'])<1:
+            flash("Password is required!", 'passwordLogin')
+            is_valid = False
+        return is_valid
+#this is the validation for the login
+    
+
+    @classmethod
+    def get_user_by_id(cls, user_id):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        data = {'id': user_id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return result[0]
